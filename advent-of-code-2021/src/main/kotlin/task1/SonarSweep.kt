1f634bbe2d5956f3a8b357b7db1030caf34d51c4
Dawid Michalowicz <dmichalowicz96@gmail.com>
2021-12-06 22:01:13 +0100

    Day 1 Part 2

--- advent-of-code-2021/src/main/kotlin/task1/SonarSweep.kt
+++ advent-of-code-2021/src/main/kotlin/task1/SonarSweep.kt
@@ -6,18 +6,22 @@ import utils.FileReader
 class SonarSweep : Problem {
 
     override fun printSolution() {
-        println("Part 1: " + calculatePart1("/task1/input/input.txt"))
+        println("Part 1: " + calculatePart1("/task1/part1/input.txt"))
+        println("Part 2: " + calculatePart2("/task1/part2/input.txt"))
     }
 
     fun calculatePart1(fileName: String): Int {
-        val measurements = FileReader.readFileAsList(fileName)
-        return measurements
-            .map { it.toInt() }
-            .filterIndexed { index, value -> if (index < measurements.size - 1) value < measurements[index + 1].toInt() else false }
-            .count()
+        val measurements = FileReader.readFileAsList(fileName).map { it.toInt() }
+        return findCountOfBiggerThanPredecessor(measurements)
     }
 
     fun calculatePart2(fileName: String): Int {
-        TODO("Did not expect two parts")
+        val measurements = FileReader.readFileAsList(fileName)
+        return findCountOfBiggerThanPredecessor(measurements.map { it.toInt() }.windowed(3).map { window -> window.sum() })
+    }
+
+    private fun findCountOfBiggerThanPredecessor(measurements: List<Int>): Int {
+        return measurements.filterIndexed { index, value -> if (index < measurements.size - 1) value < measurements[index + 1] else false }
+            .count()
     }
 }
--- advent-of-code-2021/src/test/kotlin/task1/SonarSweepTest.kt
+++ advent-of-code-2021/src/test/kotlin/task1/SonarSweepTest.kt
@@ -3,17 +3,29 @@ package task1
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
+private const val _TEST_INPUT_TXT = "/testInput.txt"
+
 internal class SonarSweepTest {
 
     private val sonarSweep: SonarSweep = SonarSweep()
 
     @Test
-    fun testSonarSweep() {
+    fun testSonarSweepPart1() {
         //given
         val expectedResult = 7
         //when
-        val result = sonarSweep.calculatePart1("/testInput.txt");
+        val result = sonarSweep.calculatePart1(_TEST_INPUT_TXT);
         //then
         assertEquals(expectedResult, result)
+    }
+
+    @Test
+    fun testSonarSweepPart2() {
+        //given
+        val expectedResult = 5
+        //when
+        val result = sonarSweep.calculatePart2(_TEST_INPUT_TXT)
+        //then
+        assertEquals(expectedResult,result)
     }
 }
